31e217242346cbc2c9e9ca6db54135671afd8d0f
sixfwa <sixfwali@gmail.com>
2024-04-18 09:50:45 +0100

    complete code

--- src/components/ShinyButton.tsx
+++ src/components/ShinyButton.tsx
@@ -1,5 +1,34 @@
+import { motion } from "framer-motion";
+
 const ShinyButton = () => {
-  return <button>Start now</button>;
+  return (
+    <motion.button
+      initial={{ "--x": "100%", scale: 1 }}
+      animate={{ "--x": "-100%" }}
+      whileTap={{ scale: 0.97 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 1,
+        type: "spring",
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: "spring",
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+      className="px-6 py-2 rounded-md relative radial-gradient"
+    >
+      <span className="text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask">
+        Start now
+      </span>
+      <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
+    </motion.button>
+  );
 };
 
 export default ShinyButton;
